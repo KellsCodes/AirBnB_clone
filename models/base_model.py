42d3f5d75c250383a17a7f4a03027e1d3bf84e5f
KellsCodes <nworjiifeanyi@gmail.com>
2023-02-12 12:27:41 +0100

    fixing bug on basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,9 @@ class BaseModel:
             returns a dictionary containing all 
             keys/values of __dict__ of the instance
         """
+        my_object = {}
+        for key, value in self.__dict__.items():
+            my_object[key] = value
 
-        self["__class__"] = self.__class__.__name__
-        return self.__dict__
+        my_object["__class__"] = self.__class__.__name__
+        return my_object
