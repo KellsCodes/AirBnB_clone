0fcb51a5acccb3aaac2aa5e5976a69693da9f5b9
KellsCodes <nworjiifeanyi@gmail.com>
2023-02-12 13:44:28 +0100

    update on baseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,12 @@ import uuid
 import datetime
 
 """
-    Base model for all other classes
-    defines all common attributes/methods for other classes
-    
-    import datetime
-    datetime.datetime.utcnow().isoformat()
+Base model for all other classes
+defines all common attributes/methods for other classes
 """
 
 
 def dateTime():
-    # return datetime.datetime.utcnow()
     return datetime.datetime.utcnow().isoformat()
 
 
@@ -22,8 +18,8 @@ class BaseModel:
 
     def __init__(self):
         """
-            initializes the base model with unique id generated with uuid
-            date the instance is created, date it is modified
+        initializes the base model with unique id generated with uuid
+        date the instance is created, date it is modified
         """
         self.id = str(uuid.uuid4())
         self.created_at = dateTime()
@@ -31,19 +27,18 @@ class BaseModel:
 
     def save(self):
         """
-            Instance method
-            saves/updates the created object instance
+        Instance method
+        saves/updates the created object instance
         """
         self.updated_at = dateTime()
 
     def to_dict(self):
         """
-            returns a dictionary containing all 
-            keys/values of __dict__ of the instance
+        returns a dictionary containing all 
+        keys/values of __dict__ of the instance
         """
         my_obj = {}
         for key, value in self.__dict__.items():
-            print("{}: {}\n".format(key, value))
             my_obj[key] = value
 
         my_obj['__class__'] = self.__class__.__name__
